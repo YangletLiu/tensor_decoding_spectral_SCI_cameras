0cadec1bb748a6dc797e84857f8a50be3a932974
hd2436 <54899335+hd2436@users.noreply.github.com>
2020-05-31 09:53:59 +0800

    Add files via upload

---
+++ NGmeet_Reconstruction.m
@@ -0,0 +1,118 @@
+function [E_Img PSNR SSIM]   =  NGmeet_Reconstruction( par,rate, A, At  )
+
+y                =    par.y;
+% rec_im0          =    DCT_CIR( y, par, A, At );
+% load rec_im0.mat
+% load CAVE_20.mat
+% load WDC_20.mat
+load Pavia_5.mat
+% O_Img            =    par.ori_im;
+rec_im           =    rec_im0;
+AtY              =    At(y);
+beta             =    par.beta;  %0.01;
+
+[h, w]           =    size(rec_im);  % size of the 2D image
+
+Par   = ParSetH_reconstruction(5,w,rate);
+N_Img = reshape(rec_im,[sqrt(h) sqrt(h) w]);
+delta =1;
+E_Img            = N_Img;                                                         % Estimated Image
+[Height, Width, Band]  = size(E_Img);  %% size of the 3D image
+N = Height*Width;
+TotalPatNum      = (Height-Par.patsize+1)*(Width-Par.patsize+1);         % Total Patch Number in the image
+Average          = mean(N_Img,3);                      % Calculate the average band for fast spatial non-local searching
+[Neighbor_arr, Num_arr, Self_arr] =	NeighborIndex(Average, Par);   
+% PreCompute all the patch index in the searching window 
+for iter = 1 : Par.Iter
+%First step: spectral dimension reduction 
+   k_subspace = min(Par.k_subspace+delta*(iter-1),30);
+   Y = reshape(E_Img, N, Band)';
+   E_Img1 = E_Img;
+   [E,~,~]= svd(Y,'econ');
+    E=E(:,1:k_subspace);
+
+    E_Img = reshape((E'*Y)', Height,Width, k_subspace);
+    N_Img1 = reshape((E'*reshape(N_Img, N, Band)')', Height,Width, k_subspace); %%% add change N_Img1 as N_Img 
+    Band1=k_subspace;
+% %non-local patch grouping and noise estimation
+    Average             =   mean(E_Img,3);
+    [CurPat, Mat, Sigma_arr]	=	Cub2Patch( E_Img, N_Img1, Average, Par );
+
+    if (mod(iter-1,2)==0)
+        Par.patnum = max(Par.patnum - 10,round(Par.patnum/2));                                          % Lower Noise level, less NL patches
+        NL_mat  =  Block_matching(Mat, Par, Neighbor_arr, Num_arr, Self_arr);  % Caculate Non-local similar patches for each
+        if(iter==1)
+            Sigma_arr = Par.nSig * ones(size(Sigma_arr))*sqrt(k_subspace/Band);                      % First Iteration use the input noise parameter
+        end
+    end
+%     time2=toc
+% non-local low-rank denoising
+    [Spa_EPat, Spa_W]    =  NLPatEstimation( NL_mat, Self_arr, Sigma_arr, CurPat, Par); 
+% reconstruct patches to 3-D image
+    [Spa_Img, Spa_Wei]   =  Patch2Cub( Spa_EPat, Spa_W, Par.patsize, Height, Width, Band1 );       % Patch to Cubic
+    E_Img = Spa_Img./Spa_Wei;
+    E_Img = reshape(reshape(E_Img, Height*Width, k_subspace)*E',Height,Width, Band);
+
+% reconstruction
+        b              =   AtY + beta * E_Img(:);
+       [X flag0]       =   pcg( @(x) Afun(x, At, A, beta), b, 0.5E-6, 400, [], [], E_Img(:));          
+        E_Img              =   reshape(X, Height, Width, Band); 
+
+    
+% time3 = toc
+% estimation, can be ignored to speed up
+    PSNR     =   csnr( reshape(E_Img,h,w), par.ori_im, 0, 0 );
+    fprintf( 'NGmeet CS Reconstruction, Iter %d : PSNR = %f \n', iter, PSNR );
+%     if iter<Par.Iter
+%     E_Img = 0.1*N_Img+0.9*E_Img;
+%     else
+%     end
+end
+if isfield(par,'ori_im')
+    PSNR     =   csnr( reshape(E_Img,h,w), par.ori_im, 0, 0 );
+    SSIM      =  cal_ssim( reshape(E_Img,h,w), par.ori_im, 0, 0 );
+end
+return;
+
+
+%======================================================
+function  y  =  Afun(x, At, A, eta)
+y      =   At( A(x) ) + eta*x;  % eta * (Wei.*x);
+return;
+
+
+%====================================================================
+% Compressive Image Recovery Using DCT 
+%--------------------------------------------------------------------
+function  Rec_im0    =   DCT_CIR( y, par, A, At )
+ori_im      =    par.ori_im;
+[h w]       =    size(ori_im);
+im          =    At( y );
+im          =    reshape(im,[h w]);
+
+lamada      =    1.5;  % 1.8, 1.2-1.7
+b           =    par.win*par.win;
+D           =    dctmtx(b);
+
+for k   =  1:1
+    f      =   im;
+    for  iter = 1 : 300 %% 300   
+        
+        if (mod(iter, 50) == 0)
+            if isfield(par,'ori_im')
+                PSNR     =   csnr( f, par.ori_im, 0, 0 );                
+                fprintf( 'DCT Compressive Image Recovery, Iter %d : PSNR = %f\n', iter, PSNR );
+            end
+        end
+        
+        for ii = 1 : 3
+            fb        =   A( f(:) );
+            f         =   f + lamada.*reshape(At( y-fb ), h, w);
+        end        
+        f          =   DCT_thresholding( f, par, D );
+    end
+    im     =  f;
+end
+Rec_im0   =  im;
+return;
+
